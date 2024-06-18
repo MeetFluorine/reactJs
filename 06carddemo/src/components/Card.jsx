45ae59917b0e21ba22c3d55a3b9688f218380269
Chandan Yadav <103582224+MeetFluorine@users.noreply.github.com>
2024-06-18 23:39:03 +0530

    change

--- 06carddemo/src/components/Card.jsx
+++ 06carddemo/src/components/Card.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable react/prop-types */
 // import React from 'react'
+import { useState } from 'react'
 import Price from './Price'
 export default function Card(props) {
+  const [title,setTitle]= useState(props.title)
   const response=()=>{
-    alert("Thank you for buying")
+   setTitle("popcorn")
   }
   return (
     <div className='mb-4 flex bg-teal-300 w-[400px] h-[150px] rounded-3xl justify-evenly items-center gap-6 '>
       <Price price={props.price}/>
-      <h1 className='text-3xl text-red-500 font-extrabold '>{props.title}</h1>
+      <h1 className='text-3xl text-red-500 font-extrabold '>{title}</h1>
       <button 
       onClick={response}
       className='border px-2 py-2 rounded  bg-blue-400 text-white font-extrabold '>Buy Now</button>
