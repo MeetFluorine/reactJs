0d072f1b75b088d8702752fb40bc28dc1f8d7ef7
Chandan Yadav <103582224+MeetFluorine@users.noreply.github.com>
2024-06-18 13:02:29 +0530

    add existing file

---
+++ 03counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  const addValue =()=>{
+      setCount(count+1);
+  }
+  const removeValue =()=>{
+      setCount(count-1);
+  }
+
+  return (
+    <>
+      <h1> practice counter</h1>
+      <h2>click on button to increase & decrease</h2>
+      <h3>{count}</h3>
+      <button
+      onClick={addValue}
+      >inc.  {count}</button>
+      <br/>
+      <br/>
+      <button
+      onClick={removeValue}
+      >dec. {count}</button>
+      <p>footer {count}</p>
+    </>
+  )
+}
+
+export default App
